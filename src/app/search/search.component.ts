4ddc667102b2668a4251b81b279f41b16dde5a29
Pierre Beaufaron <p.beaufaron@gmail.com>
2024-10-17 13:51:58 +0200

    Facoriser searchbar pr traiter affichage ds app

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CarService } from '../services/car.service';
+import { Car } from '../app.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule, ReactiveFormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  private readonly carService = inject(CarService)
+
+  searchForm: FormGroup
+  car: Car[]
+
+  @Output() formSubmitted: EventEmitter<Car[]> = new EventEmitter<Car[]>()
+
+  ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      search: new FormControl('', [
+        Validators.minLength(3), 
+        Validators.required
+      ])
+    })
+  }
+
+  onSubmitSearch() {
+    const { search } = this.searchForm.value
+    this.car = this.carService.getCars()
+    this.car = this.car.filter(c => c.brandComp.toLowerCase().includes(search))
+    this.formSubmitted.emit(this.car)
+  }
 
 }
